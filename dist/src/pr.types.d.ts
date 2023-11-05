2c1c42a4c8926084ce2b0bba55f8c5c410078d02
BuddhikaGeasman <bgeasman@gmail.com>
2023-11-05 14:07:48 -0600

    👷 CI:  Build dist

---
+++ dist/src/pr.types.d.ts
@@ -0,0 +1,8 @@
+import { Octokit } from '@octokit/core';
+import { Api } from '@octokit/plugin-rest-endpoint-methods/dist-types/types';
+export type PRDetails = {
+    oktokit: Octokit & Api;
+    owner: string;
+    repoName: string;
+    prNumber: number;
+};
